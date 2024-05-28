23d535f0ea409139600ce0ddcfd9858b136a75f7
Eduardo Loeza <eduardoloeza@Eduardos-MacBook-Air.local>
2024-05-27 23:40:21 -0600

    modificado TrainerList

--- src/components/TrainerList.tsx
+++ src/components/TrainerList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { PokemonTrainer } from "../interfaces/PokemonTrainer";
 import axios from "axios";
 import { Link } from "react-router-dom";
