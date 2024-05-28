fea19583ee6f7e94f97eb731179bbd7e3f8b4125
Eduardo Loeza <84247208+eduardoLOEZ@users.noreply.github.com>
2024-05-28 08:21:52 -0600

    cambios en la validacion

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,16 +1,36 @@
 import { PokemonTrainer } from "../interfaces/PokemonTrainer";
 
+/**
+ * validateTrainer Function
+ *
+ * Función que valida los datos de un entrenador Pokémon.
+ * Se asegura de que los campos cumplan con los requisitos mínimos de longitud y formato.
+ *
+ * @param {PokemonTrainer} trainer - Objeto que representa los datos del entrenador a validar.
+ * @returns {Object} errors - Objeto que contiene los mensajes de error para cada campo no válido.
+ */
+
 export const validateTrainer = (trainer: PokemonTrainer) => {
   const errors: { [key: string]: string } = {};
+
+  // Validación del campo 'firstName'
   if (!trainer.firstName || trainer.firstName.length < 2) {
     errors.firstName = "El nombre debe tener al menos 2 caracteres";
   }
+
+  // Validación del campo 'lastName'
   if (!trainer.lastName || trainer.lastName.length < 2) {
     errors.lastName = "Los apellidos deben tener al menos 2 caracteres";
   }
+
+  // Validación del campo 'phoneNumber'
+  // El número de teléfono debe tener 10 dígitos
   if (!trainer.phoneNumber || !/^\d{10}$/.test(trainer.phoneNumber)) {
     errors.phoneNumber = "El número de teléfono debe tener 10 dígitos";
   }
+
+  // Validación del campo 'gymBadges'
+  // El número de medallas debe estar entre 0 y 8
   if (trainer.gymBadges < 0 || trainer.gymBadges > 8) {
     errors.gymBadges = "El número de medallas debe estar entre 0 y 8";
   }
